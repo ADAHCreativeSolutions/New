2294a54c9e9128c35b1dd374061c5e31957087a2
shanboy <prashasoft@gmail.com>
2013-12-21 03:56:04 -0800

    Home Page Conversions

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="Title" content="... ">
+<meta name="description" content="...">
+<meta name="keywords" content="...">
+
+<link href="application/images/favicon.ico" rel="shortcut icon" type="image/x-icon" />
+<link href="application/css/bootstrap.min.css"  rel="stylesheet" type="text/css"/>
+
+<link rel="stylesheet" type="text/css" href="application/css/style.css"/>
+
+<script type="text/javascript" src="application/js/jquery-1.10.1.min.js"></script>
+<script type="text/javascript" src="application/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="application/js/idangerous.swiper-2.1.min.js"></script>
+<script type="text/javascript" src="application/js/script.js"></script>
+
+ <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+
+<title>VIP PLANS HOME</title>
+
+</head>
+
+
+<body>
+
+	<div id="header">
+		<div class="container">
+			<div class="col-md-2"><div class="logo">
+				<img src="application/images/logo.jpg"/>
+
+			</div></div>
+			<div class="col-md-6"><ul id="mainMenu">
+			<li><a href="#">Home</a></li>
+			<li><a href="#">All Venues</a></li>
+			<li><a href="#">Top Events</a></li>
+			<li><a href="#">My Bookings</a></li>
+				<li><a href="#">Contact</a></li>
+		</ul></div>
+			<div class="col-md-4">
+				<ul class="subMenu">
+			<li><a href="#">My Account</a></li>
+			<li><a href="#">Login</a></li>
+			<li><a href="#">Register</a></li>
+
+		</ul>
+
+			</div>
+</div>
+	</div>
+
+	<div id="vipslides">
+		<div class="right_arrow"></div>
+			<div class="left_arrow"></div>
+
+			<div id="searchBox">
+				<form>
+					<div class="sideBox"></div>
+					
+					<select>
+						<option>Select</option>
+					</select>
+
+						<select>
+						<option>Select</option>
+					</select>
+
+						<select>
+						<option>Select</option>
+					</select>
+				
+				
+
+				
+</form>
+			</div>
+
+
+		<div id="slides">
+			<img src="application/images/slide/1.jpg"/>
+
+		</div>
+	</div>
+	
+<div id="home-body">
+	<p>&nbsp;</p>
+	<div class="h1Title">
+		<h1>Top Events</h1>
+
+	</div>
+
+</div>
+
+<div id="footer">
+	<div class="container">
+		<div class="col-md-4"><p>© 2013 VIPLANS LTD. All Rights Reserved.</p></div>
+		<div class="col-md-4">icons</div>
+		<div class="col-md-4">
+			<a href="#">Home</a>
+			<a href="#">All Venues</a>
+			<a href="#">My Bookings</a>
+			<a href="#">Contact</a>
+
+		</div>
+
+
+	</div>
+
+</div>
+
+</body>
